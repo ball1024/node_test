d6d1786b22ab9b6b2571159908e94aa8ba7ac535
ball1024 <kisekung6@gmail.com>
2025-02-05 21:17:15 +0700

    commit first

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const chalk = require('chalk').default;
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'))
+
+app.get("/", (req, res) => {
+    res.send("Hello BALl");
+    
+})
+
+app.listen(port, () =>  {
+   debug("Listening on Port :" + chalk.red(port));
+})
